6469b8931adca5f38c3059ecec91f72b723a3430
OdaiJawabreh <odaijawabreh.oj@gmail.com>
2021-12-28 00:37:57 +0200

    login doctor and pataint

--- client/boocking/src/App.js
+++ client/boocking/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import SignSelect from './Components/SignSelect/SignSelect'
+import React, { createContext, useState } from "react";
+import SignSelect from "./Components/SignSelect/SignSelect";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignUpDoct from './Components/Auth/SignupDoctor/SignUpDoct';
-import LoginDoctor from './Components/Auth/Login/LoginDoctor';
-import LoginPat from './Components/Auth/Login/LoginPat';
-import SignUpPat from './Components/Auth/SignupPataint/SignUpPat';
+import SignUpDoct from "./Components/Auth/SignupDoctor/SignUpDoct";
+import LoginDoctor from "./Components/Auth/Login/LoginDoctor";
+import LoginPat from "./Components/Auth/Login/LoginPat";
+import SignUpPat from "./Components/Auth/SignupPataint/SignUpPat";
+export const tokenContext = createContext();
+
 function App() {
+  const [token, setToken] = useState();
+  const state = { token, setToken };
   return (
     <div>
-      
+      <tokenContext.Provider value={state}>
       <Router>
         <Routes>
-          <Route exact path="/signselect" element={<SignSelect/>} />
-          <Route exact path="/doctor" element={<SignUpDoct/>} />
-          <Route exact path="/pat" element={<SignUpPat/>} />
-          <Route exact path="/loginDoctor" element={<LoginDoctor/>} />
-          <Route exact path="/LoginPat" element={<LoginPat/>} />
+          <Route exact path="/signselect" element={<SignSelect />} />
+          <Route exact path="/doctor" element={<SignUpDoct />} />
+          <Route exact path="/pat" element={<SignUpPat />} />
+          <Route exact path="/loginDoctor" element={<LoginDoctor />} />
+          <Route exact path="/LoginPat" element={<LoginPat />} />
         </Routes>
       </Router>
+      </tokenContext.Provider >
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- client/boocking/src/Components/Auth/Login/LoginDoctor.js
+++ client/boocking/src/Components/Auth/Login/LoginDoctor.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React , {useState ,useContext}from "react";
+import "./login.css";
+import { tokenContext } from "../../../App";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LoginDoctor() {
-    return (
-        <div>
-            login
-        </div>
-    )
+  const history = useNavigate();
+  const state = useContext(tokenContext);
+  console.log(state);
+  const [pass, setPass] = useState("");
+  const [email, setEmail] = useState("");
+  function login(e) {
+    e.preventDefault();
+    let data = {
+      email,
+      pass,
+    };
+    console.log(data);
+    axios.post("http://localhost:5000/login/doctor", data).then((result) => {
+      localStorage.setItem("token", result.data.token);
+      state.setToken(result.data.token);
+      history("/");
+    });
+  }
+  return (
+    <div className="">
+      <form className="" onSubmit={login}>
+        <h2 className="">Please login</h2>
+        <input
+          type="text"
+          className="form-control"
+          name="username"
+          placeholder="Email Address"
+          required=""
+          autoFocus=""
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          className="l"
+          name="password"
+          placeholder="Password"
+          required=""
+          onChange={(e) => {
+            setPass(e.target.value);
+          }}
+        />
+
+        <button className="" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default LoginDoctor
+export default LoginDoctor;
